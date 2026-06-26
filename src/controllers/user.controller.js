6c42a70963c8219acaa7d409cb694ec0840e9fa0
karm <karmsorathiya532@gmail.com>
2026-06-26 16:47:25 +0530

    register controller added and bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,75 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/fileupload.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler( async(req, res) => {
-    res.status(200).json({
-        message:"OK"
+    // get user data from frontend
+    // validation checks
+    // ckeck if user already exists: username , email
+    // check for images , check for avatar
+    // upload them to Cloudinary , avatar
+    // create user object - create entry in db
+    // remove password and refresh token fields from response 
+    // check for user creation
+    // return res 
+
+    const {fullname, email, password, username} = req.body
+    console.log("fullname :", fullname);
+
+    if(
+        [fullname, email, password, username].some( (field) => 
+        feild?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ email }, { username }]
     })
+
+    if( existedUser ){
+        throw new ApiError(409, "User is already exists")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage =  await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400,"Avatar is required")
+    }
+
+    const user = await User.create({
+        fullname,
+        email,
+        password,
+        username: username.toLowerCase(),
+        avatar: avatar.url,
+        coverImage: coverImage?.url || ""
+
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, " Something wnet Wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
+
+
 })
 
 export { registerUser } 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,5 @@
+import dns from 'dns';
+dns.setServers(['8.8.8.8' || '1.1.1.1']);
 import mongoose, {Schema} from 'mongoose'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
@@ -48,7 +50,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -57,27 +59,24 @@ userSchema.methods.isPasswordCorrect = async function(password){
 }
 
 userSchema.methods.generateAccessToken = function(){
-    jwt.sign(
+    return jwt.sign(
         {
-            _id = this.id,
-            email = this.email,
-            username = this.username,
-            fullname = this.fullname
+            _id : this._id,
+            email : this.email,
+            username : this.username,
+            fullName : this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn : process.env.ACCESS_TOKEN_EXPIRY
         }
     )
 }
-
 userSchema.methods.generateRefreshToken = function(){
-     jwt.sign(
+    return jwt.sign(
         {
-            _id = this.id,
-            email = this.email,
-            username = this.username,
-            fullname = this.fullname
+            _id : this._id,
+            
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
